eaf8f8970f92056b356f07dc955391021dcccefb
Rudi <rudi@nightmare.haus>
2025-11-28 12:06:03 -0500

    Add HTTP REST

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module CompInvZ
 
 go 1.25.4
 
-require github.com/ibmdb/go_ibm_db v0.5.4
-
 require (
 	github.com/BurntSushi/toml v1.5.0 // direct
-	github.com/ibmruntimes/go-recordio/v2 v2.0.0-20250505215253-37ac59a214d6 // indirect
+	github.com/gorilla/mux v1.8.1 // direct
+	github.com/ibmdb/go_ibm_db v0.5.4
 )
+
+require github.com/ibmruntimes/go-recordio/v2 v2.0.0-20250505215253-37ac59a214d6 // indirect
--- main.go
+++ main.go
@@ -3,60 +3,64 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net"
-	"os"
 	"time"
 
 	"github.com/BurntSushi/toml"
 	_ "github.com/ibmdb/go_ibm_db"
 )
 
-type CompInvConfig struct {
-	HostName       string
-	Database       string
-	Port           int
-	UID            string
-	PWD            string
-	Heartbeat      int
-	InventoryTable string
-	ServiceTable   string
-	UptimeTable    string
-	TargetSchema   string
-}
-
 var (
 	activeConfig CompInvConfig
 	db           *sql.DB
+	out          *log.Logger
 )
 
 func main() {
+	out = log.Default()
+	out.Println("Opening Config file CompInvZ.toml")
 	f := "CompInvZ.toml"
 	_, err := toml.DecodeFile(f, &activeConfig)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		out.Fatal(err)
 	}
+	out.Println("Config active, opening Db2 connection")
 	con := fmt.Sprintf("HOSTNAME=%s;DATABASE=%s;PORT=%d;UID=%s;PWD=%s",
 		activeConfig.HostName, activeConfig.Database, activeConfig.Port,
 		activeConfig.UID, activeConfig.PWD)
 	db, err = sql.Open("go_ibm_db", con)
 	if err != nil {
-		fmt.Println(err)
+		out.Fatalf("Unable to open Db2 connection: %+v", err)
 	}
 	defer db.Close()
-	for {
-		err = testAllServices(db)
-		if err != nil {
-			fmt.Println(err)
-		}
-		time.Sleep(time.Duration(activeConfig.Heartbeat) * time.Second)
+	out.Println("Db2 connection ready")
+	if activeConfig.StartRest == 1 {
+		out.Println("Starting REST service")
+		go setupREST()
 	}
+	if activeConfig.StartChecks == 1 {
+		out.Println("Starting service checks")
+		for {
+			err = testAllServices(db)
+			if err != nil {
+				out.Printf("Error returned from testAllServices(): %+v", err)
+			}
+			time.Sleep(time.Duration(activeConfig.Heartbeat) * time.Second)
+		}
+	} else if activeConfig.StartRest == 1 {
+		for {
+			out.Println("Keeping main thread alive for REST service")
+			time.Sleep(10 * time.Minute)
+		}
+	}
+
 }
 func testService(serviceID int, serviceAddress string, servicePort string, serviceProto string) {
 	timeout := 8 * time.Second
 	active := 'D'
 	begin := time.Now()
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(serviceAddress, servicePort), timeout)
+	conn, err := net.DialTimeout(serviceProto, net.JoinHostPort(serviceAddress, servicePort), timeout)
 	if err != nil {
 		active = 'D'
 	}
@@ -68,11 +72,11 @@ func testService(serviceID int, serviceAddress string, servicePort string, servi
 	st, err := db.Prepare(fmt.Sprintf("INSERT into %s.%s(LATENCY,STATUS,SERVICE_ID) values(%d,'%c',%d)",
 		activeConfig.TargetSchema, activeConfig.UptimeTable, int(lat.Milliseconds()), active, serviceID))
 	if err != nil {
-		fmt.Println(err)
+		out.Printf("Unable to insert service entry: %+v", err)
 		return
 	}
 	_, err = st.Query()
-	fmt.Println(err)
+	out.Printf("%+v", err)
 }
 func testAllServices(db *sql.DB) error {
 	st, err := db.Prepare(fmt.Sprintf("select SERVICE_ID,ADDRESS,PORT,PROTOCOL from %s.%s",
